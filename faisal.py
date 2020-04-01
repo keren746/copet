6207bee1c5a81b14c056fc1597b0e81acb402a63
Keren <62300866+keren746@users.noreply.github.com>
2020-04-02 00:38:24 +0700

    Add files via upload

---
+++ faisal.py
@@ -0,0 +1,19 @@
+import
+import
+
+os.system(' clear')
+os.system( ' figlet arifisal
+print "===================================="
+print " Author : Faisal"
+print " alamat : cirebon"
+print " Nomeer : 085xxxxxxxxx"
+print "===================================="
+print "[+] menu pilihan [+]"
+print "[1]  Dark fbv1.7"
+pilih = raw_input (' [?] pilih : ' )
+if pilih ==  "1":
+                     os.system('git clone https://github.com/B4N954N2-ID/Dark')
+                     print "[+] penginstalan selasai"
+                     print "[+] selanjutnya ketik $ cd Dark"
+                     print "[+] lalu ketik $ python darkfb.py"
+           
