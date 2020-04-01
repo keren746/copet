58a29bd07d57355a6904e87311bf50dfa800f266
Keren <62300866+keren746@users.noreply.github.com>
2020-04-02 01:29:52 +0700

    Add files via upload

---
+++ faisal.py
@@ -0,0 +1,44 @@
+from time import sleep
+
+username = ' faisal '
+password = ' arifisal '
+
+def masuk( ) :
+                     os. system( ' clear ' )
+                     print  ( ' Masukan username dan passwo4d ' )
+                     uname = raw_input('username : ' )
+                     if uname == username:
+                                    pw = getpass.getpass( ' password : ' )
+                                    if pw == password:
+                                                  print ( 'welcome')
+                                    else:
+                                                  print ( ' password salah ! ' )
+                                                  os.  sos. system( ' xdg-open https://wa.me/6285755494099')
+                                                  exit( )
+                                                 
+                                     else:
+                                                 print ( '  username salah ! ' )
+                                                 os.  sos. system( ' xdg-open https://wa.me/6285755494099')
+                                                 exit( )
+                                 
+masuk( )
+
+import
+import
+
+os.system(' clear')
+os.system( ' figlet arifisal
+print "===================================="
+print " Author : Faisal"
+print " alamat : cirebon"
+print " Nomeer : 085xxxxxxxxx"
+print "===================================="
+print "[+] menu pilihan [+]"
+print "[1]  Dark fbv1.7"
+pilih = raw_input (' [?] pilih : ' )
+if pilih ==  "1":
+                     os.system('git clone https://github.com/B4N954N2-ID/Dark')
+                     print "[+] penginstalan selasai"
+                     print "[+] selanjutnya ketik $ cd Dark"
+                     print "[+] lalu ketik $ python darkfb.py"
+                     
